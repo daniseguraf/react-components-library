67a2c550209741b6140f21f4d2042219412e68ed
Daniel Segura <danielseguraf@gmail.com>
2021-02-04 22:30:37 -0500

    refactor(input): css variables updated

--- packages/components-react/src/components/Icon/Icon.tsx
+++ packages/components-react/src/components/Icon/Icon.tsx
@@ -5,17 +5,20 @@ import feather from 'feather-icons';
 
 export interface IconProps {
   name?: string;
-  size?: 'lg' | 'sm';
+  size?: 'lg' | 'md' | 'sm';
 }
 
 const Icon: FC<IconProps> = ({ name, size }) => {
   let iconSize;
 
   if (size === 'lg') {
-    iconSize = { width: 32, height: 32 };
+    iconSize = { width: 32, height: 32, 'stroke-width': 2 };
+  }
+  if (size === 'md') {
+    iconSize = { width: 24, height: 24, 'stroke-width': 1.5 };
   }
   if (size === 'sm') {
-    iconSize = { width: 16, height: 16 };
+    iconSize = { width: 16, height: 16, 'stroke-width': 1 };
   }
 
   return (
